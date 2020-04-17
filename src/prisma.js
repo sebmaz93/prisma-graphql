8b7dd324d1c8f1dfe1547e252d81e89359d17e6e
SebMaz <seb.maz93@icloud.com>
2020-04-17 19:40:56 +0400

    fix env vars in pkg.json

--- src/prisma.js
+++ src/prisma.js
@@ -4,8 +4,7 @@ import { fragmentReplacements } from "./resolvers";
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   endpoint: process.env.PRISMA_ENDPOINT,
-  // TODO : CHANGE TO ENV VAR LATER
-  secret: "thisismysecret",
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements
 });
 
--- src/utils/createJWT.js
+++ src/utils/createJWT.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = userId => {
-  return jwt.sign({ userId }, "mysecret", { expiresIn: "7 days" });
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "7 days" });
 };
