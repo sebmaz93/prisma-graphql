0c35e9097bb71246433e87c596cf078aabf0f528
SebMaz <seb.maz93@icloud.com>
2020-04-17 18:19:39 +0400

    feat: add envs , config dev and prod, heroku deployment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "@babel/polyfill";
+
 import { GraphQLServer } from "graphql-yoga";
 import { fragmentReplacements, resolvers } from "./resolvers";
 import prisma from "./prisma";
@@ -14,6 +16,6 @@ const server = new GraphQLServer({
   fragmentReplacements
 });
 
-server.start(() => {
+server.start({ port: process.env.PORT || 4000 }, () => {
   console.log("The server is up!");
 });
